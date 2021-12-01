6838d286e8eaeea76dd0e07771252b90b1c8fbab
laiwengyan <86708737+laiwengyan@users.noreply.github.com>
2021-12-01 14:17:49 +0800

    Add files via upload

---
+++ robberyTreausure/assets/Right-04.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Right-04" tilewidth="20" tileheight="20" tilecount="1" columns="1">
+ <image source="Right-04.png" width="32" height="32"/>
+</tileset>
