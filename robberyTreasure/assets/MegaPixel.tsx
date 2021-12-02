9e739144cbdf31b40378124be98a2ac94665f20b
laiwengyan <86708737+laiwengyan@users.noreply.github.com>
2021-12-02 22:51:53 +0800

    Delete robberyTreasure directory

--- robberyTreasure/assets/MegaPixel.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="MegaPixel" tilewidth="32" tileheight="32" tilecount="504" columns="14">
- <image source="MegaPixel.png" width="448" height="1152"/>
- <tile id="35">
-  <properties>
-   <property name="television" type="bool" value="true"/>
-  </properties>
- </tile>
-</tileset>
