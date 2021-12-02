a3187334a90ebdc6f4ae1e327c26c6987c2b676b
laiwengyan <86708737+laiwengyan@users.noreply.github.com>
2021-12-02 22:12:29 +0800

    Delete robberyTreausure directory

--- robberyTreausure/assets/Indoor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Indoor" tilewidth="10" tileheight="10" tilecount="9216" columns="96">
- <image source="Indoor.png" width="960" height="960"/>
-</tileset>
--- robberyTreausure/assets/house.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="House" tilewidth="20" tileheight="20" tilecount="90" columns="10">
- <image source="House.png" width="214" height="182"/>
-</tileset>
